b059dc5453b1e9ec94fe8f7734aa43b07421b037
khanhtranduy <130121475+khanhtranduy@users.noreply.github.com>
2024-05-15 10:53:14 +0700

    Add CORS config

--- be-java-tracing-demo/src/main/java/com/microservices/bejavatracingdemo/config/CorsConfig.java
+++ be-java-tracing-demo/src/main/java/com/microservices/bejavatracingdemo/config/CorsConfig.java
@@ -1,3 +1,10 @@
+package com.microservices.bejavatracingdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
     @Bean
